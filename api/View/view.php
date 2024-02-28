0fdaaefbc17573f915d5625c1b1317cfc72734eb
FabienSerres <fabien.serres@iut-rodez.fr>
2024-02-28 14:46:32 +0100

    Création de la vue
    Co-Authored-By: rom0raf <130551777+rom0raf@users.noreply.github.com>

---
+++ api/View/view.php
@@ -0,0 +1,8 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+
+http_response_code($status);
+echo json_encode($json, JSON_UNESCAPED_UNICODE);
+die;
