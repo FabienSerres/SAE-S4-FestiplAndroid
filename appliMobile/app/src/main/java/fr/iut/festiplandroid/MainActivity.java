806023b41b00211313458c05de028d67afff9a10
Fabien Serres <fabien.serres@iut-rodez.fr>
2024-03-05 09:14:23 +0100

    Fin de la partie connexion avec tampon

--- appliMobile/app/src/main/java/fr/iut/festiplandroid/MainActivity.java
+++ appliMobile/app/src/main/java/fr/iut/festiplandroid/MainActivity.java
@@ -1,14 +1,47 @@
 package fr.iut.festiplandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import fr.iut.festiplandroid.utils.Utils;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
+/**
+ * The main activity of the FestiPlAndroid application.
+ * Allows users to log in and access festival information.
+ */
 public class MainActivity extends AppCompatActivity {
+
+    EditText txtId;
+    EditText txtPassword;
+    TextView msgConnection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtId = findViewById(R.id.txt_id);
+        txtPassword = findViewById(R.id.txt_password);
+        msgConnection = findViewById(R.id.txt_connection);
+    }
+
+    /**
+     * Method called when a user wants to connect.
+     *
+     * @param view The view that triggered the action (in this case, a button).
+     */
+    public void connectAction(View view) {
+        String id = txtId.getText().toString();
+        String password = txtPassword.getText().toString();
+
+        boolean connected = Utils.connect(id, password);
+
+        if (connected) {
+            msgConnection.setText(R.string.connect_successful);
+        } else {
+            msgConnection.setText(R.string.connect_error);
+        }
     }
 }
---
+++ appliMobile/app/src/main/java/fr/iut/festiplandroid/utils/Utils.java
@@ -0,0 +1,23 @@
+package fr.iut.festiplandroid.utils;
+
+/**
+ * Utility class containing methods for FestiPlAndroid application.
+ */
+public class Utils {
+
+    private final static String LOGIN_DEV = "identifiant";
+    private final static String PASSWORD_DEV = "password123";
+
+    /**
+     * Check if the param given by the user are valid (for the moment they need to be equals to
+     * the LOGIN and PASSWORD _DEV) and if they are, connect him.
+     *
+     * @param id the id given by the user
+     * @param password the password given by the user
+     * @return true if the information of connection are true, false otherwise
+     */
+    public static boolean connect(String id, String password) {
+        return id.equals(LOGIN_DEV) && password.equals(PASSWORD_DEV);
+    }
+
+}
