03435f77847b2af4dffd034656dee57da4092343
FabienSerres <fabien.serres@iut-rodez.fr>
2024-03-15 11:14:20 +0100

    Added the favorites management
    Co-Authored-By: aurelsoleil <130551000+aurelsoleil@users.noreply.github.com>

--- appliMobile/app/src/main/java/fr/iut/festiplandroid/utils/CustomAdapter.java
+++ appliMobile/app/src/main/java/fr/iut/festiplandroid/utils/CustomAdapter.java
@@ -16,8 +16,10 @@ import androidx.annotation.Nullable;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.ServerError;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
 
@@ -65,12 +67,23 @@ public class CustomAdapter extends ArrayAdapter<String> {
             @Override
             public void onClick(View v) {
                 String tag = (String) button.getTag();
+                int idFestival = Utils.getFestivalId(position);
                 if (tag.equals("empty")) {
                     button.setImageResource(R.drawable.star_full);
                     button.setTag("full");
+
+                    String addFavoriteUrl = String.format(Utils.URL_API_ADD_FAV, Utils.idUser,
+                                                                                 idFestival);
+                    RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+                    requestQueue.add(requestAddFavoriteFestival(addFavoriteUrl));
                 } else if (tag.equals("full")) {
                     button.setImageResource(R.drawable.star_empty);
                     button.setTag("empty");
+
+                    String deleteFavoriteUrl = String.format(Utils.URL_API_DEL_FAV, Utils.idUser,
+                                                                                    idFestival);
+                    RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+                    requestQueue.add(requestDeleteFavoriteFestival(deleteFavoriteUrl));
                 }
             }
         });
@@ -78,5 +91,82 @@ public class CustomAdapter extends ArrayAdapter<String> {
         return listItemView;
     }
 
+    /**
+     * Creates a JsonObjectRequest for adding a festival to favorites.
+     *
+     * @param url The URL for adding the festival to favorites.
+     * @return The JsonObjectRequest for adding the festival to favorites.
+     */
+    private JsonObjectRequest requestAddFavoriteFestival(String url) {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.PUT, url, null,
+            new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    Toast.makeText(getContext(), R.string.msg_success_add_fav,
+                                                 Toast.LENGTH_SHORT).show();
+                }
+            },
+            new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    if (error instanceof ServerError && error.networkResponse != null) {
+                        int statusCode = error.networkResponse.statusCode;
+                        if (statusCode == 500 || statusCode == 400) {
+                            Toast.makeText(getContext(),
+                                    R.string.msg_error_add_fav,
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }
+            }) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Content-Type", "application/json");
+                headers.put(Utils.API_KEY_NAME, Utils.apiKeyUser);
+                return headers;
+            }
+        };
+        return jsonObjectRequest;
+    }
+
+    /**
+     * Creates a JsonObjectRequest for deleting a festival from favorites.
+     *
+     * @param url The URL for deleting the festival from favorites.
+     * @return The JsonObjectRequest for deleting the festival from favorites.
+     */
+    private JsonObjectRequest requestDeleteFavoriteFestival(String url) {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.DELETE, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Toast.makeText(getContext(), R.string.msg_success_delete_fav,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        if (error instanceof ServerError && error.networkResponse != null) {
+                            int statusCode = error.networkResponse.statusCode;
+                            if (statusCode == 500 || statusCode == 400) {
+                                Toast.makeText(getContext(),
+                                        R.string.msg_error_delete_fav,
+                                        Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Content-Type", "application/json");
+                headers.put(Utils.API_KEY_NAME, Utils.apiKeyUser);
+                return headers;
+            }
+        };
+        return jsonObjectRequest;
+    }
 }
 
