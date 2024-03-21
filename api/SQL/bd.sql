6aee4046489613ecccf5e78e68fe82308d7f3802
FabienSerres <fabien.serres@iut-rodez.fr>
2024-03-21 18:49:02 +0100

    Fix bd.sql

--- api/SQL/bd.sql
+++ api/SQL/bd.sql
@@ -292,10 +292,10 @@ INSERT INTO SpectacleDeFestival (idSpectacle, idFestival)
 VALUES
     (1, 1),
     (2, 1),
-    (5, 1);
+    (5, 1),
     (3, 2),
     (4, 2),
-    (5, 2);
+    (5, 2),
     (2, 3),
     (3, 3),
     (5, 3);
